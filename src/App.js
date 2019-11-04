907ef96219279a788538ad8a2f4487b294f7f438
unknown <phuocduc2012@gmail.com>
2019-11-04 09:22:45 +0700

    test netlify again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import {Route,Switch} from 'react-router-dom'
-import './App.css';
-import Login from './View/Login'
-import Product from './View/Product'
-import Candidates from './View/Candidates';
-import Contact from './View/Contact';
-import AboutMe from './View/AboutMe';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./View/Login";
+import Product from "./View/Product";
+import Candidates from "./View/Candidates";
+import Contact from "./View/Contact";
+import AboutMe from "./View/AboutMe";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 function App() {
   return (
-    
     <Switch>
 
       <Route path="/" exact component={Login}></Route>
@@ -18,11 +17,8 @@ function App() {
       <Route path="/candidates/:id/:edit" exact component={Candidates}></Route>
       <Route path="/contact" exact component={Contact}></Route>
       <Route path="/aboutMe" exact component={AboutMe}></Route>
-
     </Switch>
-    // <Route></Route>
-    // <Route></Route>
-    // <Route></Route>
+    
   );
 }
 
